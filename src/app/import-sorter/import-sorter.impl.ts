adc4a1e277cb13f2ad53ccc3b5d566528d635cec
aziznal <aziznal.dev@gmail.com>
2022-05-27 10:40:00 +0300

    refactor: minor cleanup and name changes

--- src/app/import-sorter/import-sorter.impl.ts
+++ src/app/import-sorter/import-sorter.impl.ts
@@ -4,7 +4,7 @@ import { IExtensionSettings } from '../extension-settings/extension-settings.int
 import { IImportSorter, SortingResult } from './import-sorter.interface';
 
 import { ImportUtils } from '../import-utils/import-utils';
-import { ImportGroup } from '../types/import-group';
+import { StatementGroup } from '../types/statement-group';
 
 /**
  * Sorts imports using a basic grouping algorithm.
@@ -14,13 +14,13 @@ export class ImportSorter implements IImportSorter {
     constructor(@inject('IExtensionSettings') public settings: IExtensionSettings) {}
 
     sortImports(rawDocumentBody: string): SortingResult {
-        const formattedImports = ImportUtils.extractFormattedImports(rawDocumentBody);
+        const strippedImports = ImportUtils.extractFormattedImports(rawDocumentBody);
 
-        if (formattedImports.length === 0) {
+        if (strippedImports.length === 0) {
             return { sortedImports: '', firstRawImportIndex: -1, lastRawImportIndex: -1 }; // nothing to sort
         }
 
-        const groups = this.groupImports(formattedImports);
+        const groups = this.groupImports(strippedImports);
 
         groups.forEach((group) => {
             group.imports = ImportUtils.sortAlphabetically(group.imports);
@@ -35,41 +35,39 @@ export class ImportSorter implements IImportSorter {
         };
     }
 
-    private groupImports(importStatements: string[]): ImportGroup[] {
-        let copiedImportStatements = importStatements.slice();
+    private groupImports(strippedImports: string[]): StatementGroup[] {
+        let statements = strippedImports.slice();
 
-        const importGroups: ImportGroup[] = [];
+        const groups: StatementGroup[] = [];
 
         this.settings.sortingRules.forEach((preference) => {
-            const matchingStatements = copiedImportStatements.filter((statement) =>
+            const matchingStatements = statements.filter((statement) =>
                 preference.regex.test(ImportUtils.getImportPath(statement))
             );
 
-            importGroups.push({
+            groups.push({
                 groupRegex: preference.regex,
                 order: preference.order,
                 imports: matchingStatements,
             });
 
             // remove matching statements so they don't match again with later regex
-            copiedImportStatements = copiedImportStatements.filter(
-                (statement) => !matchingStatements.includes(statement)
-            );
+            statements = statements.filter((statement) => !matchingStatements.includes(statement));
         });
 
         // if there are imports that don't match anything, throw them at the end
-        if (copiedImportStatements.length > 0) {
-            importGroups.push({
+        if (statements.length > 0) {
+            groups.push({
                 groupRegex: /.*/gm,
-                imports: copiedImportStatements,
-                order: Math.max(...importGroups.map((group) => group.order)) + 1,
+                imports: statements,
+                order: Math.max(...groups.map((group) => group.order)) + 1,
             });
         }
 
-        return importGroups;
+        return groups;
     }
 
-    private flattenImportGroups(groups: ImportGroup[]): string {
+    private flattenImportGroups(groups: StatementGroup[]): string {
         groups = groups.sort((groupA, groupB) => groupA.order - groupB.order);
 
         const concattedGroups = groups
